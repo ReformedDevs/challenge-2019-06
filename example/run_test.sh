c66b5d98e6658089652ed112fcdeb6378ab2ec94
dnesbitt03qub <dnesbitt03@qub.ac.uk>
2019-05-29 22:16:28 +0100

    Add build and run script

---
+++ example/run_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Put your name, the source (to be counted) and the command used run you solution
+NAME=plusuncold
+SOURCE=src/main.rs
+RUN_COMMAND=./target/release/fact
+
+# Uncomment this line
+# echo "$USER, `"$RUN_COMMAND"`, `wc -c < $SOURCE`"
