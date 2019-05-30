08ecb76111ed829dab4aba8fbc60835df2181814
plusuncold <10950294+plusuncold@users.noreply.github.com>
2019-05-30 09:51:46 +0100

    Update run_test.sh

--- example/run_test.sh
+++ example/run_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Put your name, the source (to be counted) and the command used run you solution
+# Put your name, the source (for the characters to be counted) and the command used to run your solution
+# e.g. For plusuncold's rust version
 NAME=plusuncold
 SOURCE=src/main.rs
 RUN_COMMAND=./target/release/fact
 
 # Uncomment this line
-# echo "$USER, `"$RUN_COMMAND"`, `wc -c < $SOURCE`"
+# echo "$NAME, `"$RUN_COMMAND"`, `wc -c < $SOURCE`"
