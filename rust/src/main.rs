ce3c2701aac97aa6591b32546a90bf29313de76d
dnesbitt03qub <dnesbitt03@qub.ac.uk>
2019-05-30 20:18:00 +0100

    move rust solution to rust-plusuncold and improve rust solution

--- rust/src/main.rs
+++
@@ -1,1 +0,0 @@
-use rug::{Assign,Integer};fn main(){let mut b = Integer::new();b.assign(1);for i in 2..100{b *= i;}let mut t=0;for c in b.to_string_radix(10).chars(){t+=c.to_digit(10).unwrap();}print!("{}",t);}
