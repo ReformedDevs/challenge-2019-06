6518321b24ac39f01a635012b161b8b697bc75cf
leebehrens <lee@boolion.com>
2019-06-17 22:18:43 -0500

    Create run_test.sh

---
+++ applesoft-lee/run_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Put your name, the source (for the characters to be counted) and the command used to run your solution
+# e.g. For plusuncold's rust version
+NAME="Lee Behrens"
+SOURCE=648
+RUN_COMMAND=`echo $SOURCE`
+
+# Uncomment this line
+echo "$NAME, $RUN_COMMAND, `echo 130`"
